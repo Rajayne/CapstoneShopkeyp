90902b9ded0bf149ca04e4da7bb84440d894f59a
Rajayne <ryann.j.nealey@gmail.com>
2023-06-08 10:11:34 -0700

    Create express server and connect to database

---
+++ db.js
@@ -0,0 +1,15 @@
+const { Client } = require("pg");
+const { user, password } = require("../dbPassword");
+
+let DB_URI =
+  process.env.NODE_ENV === "test"
+    ? `postgresql://${user}:${password}@localhost:5432/shopkeyp_test`
+    : `postgresql://${user}:${password}@localhost:5432/shopkeyp`;
+
+let db = new Client({
+  connectionString: DB_URI,
+});
+
+db.connect();
+
+module.exports = db;
