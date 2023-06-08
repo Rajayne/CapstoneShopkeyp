59ab839bd4b9c8bda370647893a7e358fd459a06
Rajayne <ryann.j.nealey@gmail.com>
2023-06-08 10:14:16 -0700

    Create express error handler

--- app.js
+++ app.js
@@ -3,4 +3,14 @@ const app = express();
 
 app.use(express.json());
 
+/** General error handler */
+app.use((err, req, res, next) => {
+  res.status(err.status || 500);
+
+  return res.json({
+    error: err,
+    message: err.message,
+  });
+});
+
 module.exports = app;
