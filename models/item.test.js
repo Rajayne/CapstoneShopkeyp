2bfc333d6c860eaeeb69c1cd3b4671710294f23b
Rajayne <ryann.j.nealey@gmail.com>
2023-06-09 13:17:37 -0700

    Create testItem variables and update database schema to allow items with same uuid

--- models/item.test.js
+++ models/item.test.js
@@ -1,5 +1,7 @@
+/* eslint-disable comma-dangle */
 const db = require('../db');
 const User = require('./user');
+const Item = require('./item');
 const ExpressError = require('../expressError');
 
 const {
@@ -10,16 +12,18 @@ const {
 
 let testAdmin;
 let testItem;
+let testItem2;
 
 beforeAll(async () => {
   await db.query('DELETE FROM users');
+  await db.query('DELETE FROM items');
 
   const admin = await db.query(
     `INSERT INTO users 
       (username, password, profile_image, is_admin) 
     VALUES 
       ('admin1', $1, 'adminProfile.png', true) 
-      RETURNING 
+    RETURNING 
       user_id AS "userId",
       username,
       profile_image AS "profileImage",
@@ -30,9 +34,59 @@ beforeAll(async () => {
     ['adminPass1']
   );
 
-  [testAdmin] = admin.rows.map((a) => new User(a));
+  [testAdmin] = await admin.rows.map((a) => new User(a));
+
+  const item = await db.query(
+    `INSERT INTO items 
+      (name, description, item_image, price, created_by) 
+    VALUES 
+      ('item1', 'description1',  'item1.png', 5, $1) 
+    RETURNING 
+      item_uuid AS "itemUuid",
+      item_id AS "itemId",
+      name,
+      description,
+      item_image AS "itemImage",
+      price,
+      stock,
+      purchasable,
+      created_by AS "createdBy",
+      date_created AS "dateCreated"`,
+    [testAdmin.userId]
+  );
+
+  [testItem] = await item.rows.map((i) => new Item(i));
+
+  const item2 = await db.query(
+    `INSERT INTO items 
+      (item_uuid, name, description, item_image, price, created_by) 
+    VALUES 
+      ($1, 'item1', 'description1',  'item1.png', 5, $2) 
+    RETURNING 
+      item_uuid AS "itemUuid",
+      item_id AS "itemId",
+      name,
+      description,
+      item_image AS "itemImage",
+      price,
+      stock,
+      purchasable,
+      created_by AS "createdBy",
+      date_created AS "dateCreated"`,
+    [testItem.itemUuid, testAdmin.userId]
+  );
+
+  [testItem2] = await item2.rows.map((i) => new Item(i));
 });
 
 beforeEach(commonBeforeEach);
 afterEach(commonAfterEach);
 afterAll(commonAfterAll);
+
+describe('Item Model Tests', () => {
+  test('creates testAdmin, testItem and testItem2 variables', async () => {
+    expect(testAdmin instanceof User).toBeTruthy();
+    expect(testItem instanceof Item).toBeTruthy();
+    expect(testItem2 instanceof Item).toBeTruthy();
+  });
+});
--- shopkeyp-schema.sql
+++ shopkeyp-schema.sql
@@ -2,7 +2,7 @@ CREATE TABLE users (
     user_id SERIAL PRIMARY KEY,
     username VARCHAR(25) UNIQUE,
     password text NOT NULL,
-    profile_image TEXT DEFAULT 'https://media.istockphoto.com/id/1300845620/vector/user-icon-flat-isolated-on-white-background-user-symbol-vector-illustration.jpg?s=612x612&w=0&k=20&c=yBeyba0hUkh14_jgv1OKqIH0CCSWU_4ckRkAoy2p73o=',
+    profile_image TEXT CHECK (position('.' IN profile_image) > 1) DEFAULT 'https://media.istockphoto.com/id/1300845620/vector/user-icon-flat-isolated-on-white-background-user-symbol-vector-illustration.jpg?s=612x612&w=0&k=20&c=yBeyba0hUkh14_jgv1OKqIH0CCSWU_4ckRkAoy2p73o=',
     balance INTEGER CHECK (balance >= 0) DEFAULT 0,
     is_admin BOOLEAN NOT NULL DEFAULT false,
     active BOOLEAN NOT NULL DEFAULT true,
@@ -10,33 +10,33 @@ CREATE TABLE users (
 );
 
 CREATE TABLE items (
-    item_uuid SERIAL PRIMARY KEY,
-    item_id SERIAL NOT NULL,
+    item_uuid SERIAL NOT NULL,
+    item_id SERIAL PRIMARY KEY NOT NULL,
     name TEXT NOT NULL,
     description TEXT,
     item_image TEXT CHECK (position('.' IN item_image) > 1) DEFAULT 'https://media.istockphoto.com/id/1193046540/vector/photo-coming-soon-image-icon-vector-illustration-isolated-on-white-background-no-website.jpg?s=612x612&w=0&k=20&c=4wx1UzigP0g9vJv9D_DmOxdAT_DtX5peZdoS4hi2Fqg=',
     price INTEGER CHECK (price >= 0) DEFAULT 0,
     stock INTEGER CHECK (stock >= 0) DEFAULT 0,
     purchasable BOOLEAN DEFAULT true,
-    created_by INTEGER REFERENCES users,
+    created_by INTEGER REFERENCES users ON DELETE CASCADE,
     date_created TIMESTAMP DEFAULT current_timestamp
 );
 
 CREATE TABLE transactions (
     transactionId SERIAL PRIMARY KEY,
-    from_user INTEGER NOT NULL REFERENCES users,
-    to_user INTEGER NOT NULL REFERENCES users,
+    from_user INTEGER NOT NULL REFERENCES users ON DELETE CASCADE,
+    to_user INTEGER NOT NULL REFERENCES users ON DELETE CASCADE,
     action TEXT NOT NULL,
-    item_uuid INTEGER REFERENCES items,
+    item_uuid INTEGER REFERENCES items ON DELETE CASCADE,
     quantity INTEGER CHECK (quantity >= 0) DEFAULT 0,
     total INTEGER CHECK (quantity >= 0) DEFAULT 0,
-    admin_id INTEGER REFERENCES users DEFAULT NULL,
+    admin_id INTEGER REFERENCES users ON DELETE CASCADE DEFAULT NULL,
     transaction_date TIMESTAMP DEFAULT current_timestamp
 );
 
 CREATE TABLE user_items (
-    user_id INTEGER REFERENCES users,
-    item_uuid INTEGER REFERENCES items,
+    user_id INTEGER REFERENCES users ON DELETE CASCADE,
+    item_uuid INTEGER REFERENCES items ON DELETE CASCADE,
     quantity INTEGER CHECK (quantity >= 0) NOT NULL,
     PRIMARY KEY(user_id, item_uuid)
 );
