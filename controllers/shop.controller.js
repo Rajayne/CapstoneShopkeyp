c338a9c7fbe69c77cc0d39eac7fb9fb7a87d9ada
dukelester <dukelester4@gmail.com>
2023-06-18 23:20:09 +0300

    Created tte controllers for the shop
    this will create, get item and purchase an item.

--- controllers/shop.controller.js
+++ controllers/shop.controller.js
@@ -1,1 +1,31 @@
+/* eslint-disable import/extensions */
 // The shop controllers
+import Item from '../models/item.js';
+import User from '../models/user.js';
+
+export const getItems = async (req, res, next) => {
+  try {
+    const items = await Item.purchasable();
+    return res.json(items);
+  } catch (err) {
+    return next(err);
+  }
+};
+
+export const getItemByID = async (req, res, next) => {
+  try {
+    const item = await Item.get(req.params.itemId);
+    return res.json(item);
+  } catch (err) {
+    return next(err);
+  }
+};
+
+export const createPuchaseItem = async (req, res, next) => {
+  try {
+    const transaction = await User.purchase(req.body);
+    return res.json(transaction);
+  } catch (err) {
+    return next(err);
+  }
+};
