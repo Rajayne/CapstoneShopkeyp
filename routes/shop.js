f11472e2ee687d76a8f644db74981fed9b321b0b
dukelester <dukelester4@gmail.com>
2023-06-18 23:20:47 +0300

    Imported the controlers for the shop
    this separates the logic from the route.

--- routes/shop.js
+++ routes/shop.js
@@ -2,47 +2,14 @@
 /* eslint-disable comma-dangle */
 import express from 'express';
 import { authenticateJWT, requireLogin, ensureCorrectUserOrAdmin } from '../middleware/auth.js';
-import Item from '../models/item.js';
-import User from '../models/user.js';
+import { createPuchaseItem, getItemByID, getItems } from '../controllers/shop.controller.js';
 
 const router = express.Router();
 
-router.get('/', authenticateJWT, requireLogin, async (req, res, next) => {
-  try {
-    const items = await Item.purchasable();
-    return res.json(items);
-  } catch (err) {
-    return next(err);
-  }
-});
+router.get('/', authenticateJWT, requireLogin, getItems);
 
-router.get(
-  '/item/:itemId',
-  authenticateJWT,
-  requireLogin,
-  async (req, res, next) => {
-    try {
-      const item = await Item.get(req.params.itemId);
-      return res.json(item);
-    } catch (err) {
-      return next(err);
-    }
-  }
-);
+router.get('/item/:itemId', authenticateJWT, requireLogin, getItemByID);
 
-router.get(
-  '/item/:itemId/purchase',
-  authenticateJWT,
-  requireLogin,
-  ensureCorrectUserOrAdmin,
-  async (req, res, next) => {
-    try {
-      const transaction = await User.purchase(req.body);
-      return res.json(transaction);
-    } catch (err) {
-      return next(err);
-    }
-  }
-);
+router.get('/item/:itemId/purchase', authenticateJWT, requireLogin, ensureCorrectUserOrAdmin, createPuchaseItem);
 
 export default router;
