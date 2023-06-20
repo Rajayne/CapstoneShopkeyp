4ff732536468f6eaa8b8ad9a5747cecec275d766
Rajayne <ryann.j.nealey@gmail.com>
2023-06-19 22:31:57 -0700

    Add state and validators to RegisterForm

--- shopkeyp/src/components/Register/RegisterForm.jsx
+++ shopkeyp/src/components/Register/RegisterForm.jsx
@@ -1,32 +1,57 @@
-import React from "react";
+import React, { useState } from "react";
 import { Button, Card, CardContent } from '@mui/material';
 import './RegisterForm.css'
+import { useNavigate, useParams } from 'react-router-dom';
 
-const RegisterForm = () => {
+const NewRegisterForm = () => {
+  const navigate = useNavigate();
+
+  const initialState = { username: "", password: "", retype: "" };
+  const [formData, setFormData] = useState(initialState);
+  const { username, password, retype } = formData;
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((formData) => ({ ...formData, [name]: value }));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (password !== retype) {
+      alert("Passwords do not match!");
+      return;
+    }
+    setFormData(initialState);
+    navigate('/profile')
+  };
+
+  const validate = () => !Object.values(formData)
+  .some((value) => value === 0 || value.length === 0);
+
   return (
     <Card className="RegisterForm">
-      <CardContent className="RegisterForm-form">
-      <form>
+      <CardContent className="RegisterForm-content">
+      <form onSubmit={handleSubmit} className="RegisterForm-form">
         <div className="RegisterForm-username">
-          <label for="username" className="RegisterForm-label">Username</label>
+          <label htmlFor="username" className="RegisterForm-label">Username</label>
         </div>
         <div className="RegisterForm-username">
-          <input name="username" id="username" className="RegisterForm-input" type="text" placeholder="Shopkeyp"></input>
+          <input name="username" id="username" onChange={handleChange} value={username} className="RegisterForm-input" type="text" placeholder="Shopkeyp"></input>
         </div>
         <div className="RegisterForm-password">
-          <label for="password" className="RegisterForm-label">Password</label>
+          <label htmlFor="password" className="RegisterForm-label">Password</label>
         </div>
         <div className="RegisterForm-password">
-          <input name="password" id="password" className="RegisterForm-input" type="password" placeholder="********"></input>
+          <input name="password" id="password" onChange={handleChange} value={password} className="RegisterForm-input" type="password" placeholder="********"></input>
         </div>
         <div className="RegisterForm-retype">
-          <label for="password" className="RegisterForm-label">Retype Password</label>
+          <label htmlFor="retype" className="RegisterForm-label">Retype Password</label>
         </div>
         <div className="RegisterForm-retype">
-          <input name="password" id="password" className="RegisterForm-input" type="password" placeholder="********"></input>
+          <input name="retype" id="retype" onChange={handleChange} value={retype} className="RegisterForm-input" type="password" placeholder="********"></input>
         </div>
         <div className="RegisterForm-button">
-          <Button variant="contained">Submit</Button>
+          <Button variant="contained" type="submit" disabled={!validate()}>Submit</Button>
         </div>
       </form>
       </CardContent>
@@ -34,4 +59,4 @@ const RegisterForm = () => {
   );
 };
 
-export default RegisterForm;
+export default NewRegisterForm;
